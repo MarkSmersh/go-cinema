28cf6cb7a4cb71c53a125f41e688be78e41982cd
MarkSmersh <marksmersh01@gmail.com>
2024-12-26 14:09:10 +0100

    add more methods for schemas

--- database/schemas/schemas.go
+++ database/schemas/schemas.go
@@ -37,5 +37,81 @@ func (s Schema) Init() {
 		log.Fatalln(err)
 	}
 
-	fmt.Printf("%+v", rows)
+	fmt.Printf("%+v\n", rows)
+
+	columns, err = rows.Columns()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%+v\n", columns)
+}
+
+func (s Schema) FindById(id int16) {
+	query := fmt.Sprintf("SELECT 1 FROM public.%s WHERE name = %d;", s.name, id)
+
+	rows, err := database.DB.Query(query)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%+v\n", rows)
+}
+
+// I have no idea how to implement
+// generic type for values
+// (depends on the values of s)
+
+func (s Schema) Create(col []string, val []string) {
+	var quoteValues []string
+
+	for i := 0; i < len(val); i++ {
+		quoteValues = append(quoteValues, fmt.Sprintf("'%s'", val[i]))
+	}
+
+	query := fmt.Sprintf(
+		"INSERT INTO public.%s (%s) VALUES (%s);",
+		s.name,
+		strings.Join(col, ","),
+		strings.Join(quoteValues, ","),
+	)
+
+	fmt.Println(query)
+
+	rows, err := database.DB.Query(query)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%+v\n", rows)
+}
+
+func (s Schema) Find(col string, val string) {
+	// I'm curious how does it work with int
+
+	query := fmt.Sprintf("SELECT * FROM public.%s WHERE %s = '%s'", s.name, col, val)
+
+	database.DB.Query(query)
+}
+
+func (s Schema) Query(statement string, data string, args ...any) {
+	query := fmt.Sprintf(
+		"%s FROM public.%s %s",
+		statement,
+		s.name,
+		fmt.Sprintf(data, args...),
+	)
+
+	fmt.Println(query)
+
+	rows, err := database.DB.Query(query)
+
+	if err != nil {
+		log.Fatalln()
+	}
+
+	fmt.Printf("%+v\n", rows)
 }
--- init/init.go
+++ init/init.go
@@ -1,9 +1,18 @@
 package init
 
 import (
+	"fmt"
+
 	"github.com/MarkSmersh/go-cinema/database/schemas"
 )
 
 func init() {
 	schemas.User.Init()
+
+	schemas.User.Create(
+		[]string{"name"},
+		[]string{"test"},
+	)
+
+	fmt.Println("Nice!")
 }
